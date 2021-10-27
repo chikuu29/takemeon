89d834eb719f31568c4b38697acf977f5951e3a4
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2021-10-27 20:53:29 +0530

    every this looks good

---
+++ js/fetch_search_item.js
@@ -0,0 +1,60 @@
+
+var search_result=document.getElementById('search-item');
+
+
+function get_search_result(str) {
+    
+  
+    if (str==" ") {
+        search_result.style.display="none";
+    }else{
+        
+        findsearchiem(str);
+    }
+   
+    
+  
+}
+
+function findsearchiem(input_value) {
+    search_result.style.display="block";
+    $(document).ready(function(){
+        $.ajax({
+            url:'php/fetch_search_result.php',
+            type:'POST',
+            data:{q:input_value},
+            success:function(data){
+
+                $('#searh_holder').html(data);
+                // console.log(data)
+
+
+            }
+        })
+        
+
+
+    })
+
+    var input, filter, ul, li, a, i;
+
+    // input = document.getElementById("mySearch");
+    filter = input_value.toUpperCase();
+    console.log(filter);
+    ul = document.getElementById("searh_holder");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            
+            li[i].style.display = "";
+          
+            
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+   
+}
+
+
